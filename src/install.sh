07c04c15bc4b174b477f9e976e142c35d61d9c06
Dylan Bock <bockdaa@uni.edu>
2026-02-11 16:41:17 -0600

    Updated pytorch wheels from cu118 to cu128, reconfigured offline settings, added nemotron installation requirements, upgraded cuda toolkit to 12.8, upgrade python to 3.12, removed unnecessary python packages

--- src/install.sh
+++ src/install.sh
@@ -1,11 +1,13 @@
 #!/usr/bin/env bash
 
-echo "INSTALLATION PREVENTED. DON'T RUN THIS SCRIPT UNTIL NEW MODEL HAS BEEN CHOSEN."
-exit 1
-
 set -eo pipefail
 
 ENV_NAME="ocr-env"
+CHANNEL="https://download.pytorch.org/whl/cu128"
+
+export HF_HOME="${HOME}/.hf_offline_cache"
+export TRANSFORMERS_OFFLINE=1
+export HF_HUB_OFFLINE=1
 
 echo "=== Creating temporary install directory ==="
 BUILD_ROOT="${HOME}/.tmpbuild"
@@ -25,31 +27,56 @@ trap cleanup EXIT
 
 echo "=== Creating conda environment: $ENV_NAME ==="
 source "$(conda info --base)/etc/profile.d/conda.sh"
-conda create -y -n $ENV_NAME python=3.11
+conda create -y -n "$ENV_NAME" python=3.12
 
 echo "=== Activating environment ==="
-conda activate $ENV_NAME
+conda activate "$ENV_NAME"
 
 echo "=== Install CUDA toolkit ==="
-conda install -y -c nvidia cuda-toolkit=11.8
+conda install -y -c nvidia cuda-toolkit=12.8
 export CUDA_HOME="$CONDA_PREFIX"
 export PATH="$CUDA_HOME/bin:$PATH"
 export LD_LIBRARY_PATH="$CUDA_HOME/lib:$CUDA_HOME/lib64:${LD_LIBRARY_PATH:-}"
+
+echo "=== Installing build prerequisites ==="
+conda install -y -c conda-forge git-lfs gxx_linux-64 cmake make ninja
+git lfs install
+
+python -m pip install --upgrade pip setuptools wheel
+pip install --no-cache-dir hatchling hatch-vcs pybind11[global]
+pip install --no-cache-dir -U numpy
 
 echo "=== Clearing cache ==="
 conda clean -a -y || true
 pip cache purge || true
 
-echo "=== Manually installing wheels ==="
-pip install --no-cache-dir --default-timeout=600 torch==2.6.0 torchvision==0.21.0 torchaudio==2.6.0 --index-url https://download.pytorch.org/whl/cu118
+echo "=== Install PyTorch ==="
+pip install --no-cache-dir --default-timeout=600 torch torchvision torchaudio --index-url "$CHANNEL"
 
-echo "=== Installing Markdown and PDF tools ==="
-conda install -y -c conda-forge pandoc wkhtmltopdf
+echo "=== Installing Nemotron OCR ==="
+
+# Modify this if using A30 or A100 (8.0, not 7.0)
+export TORCH_CUDA_ARCH_LIST="7.0"
+
+if [ ! -d "nemotron-ocr-v1" ]; then
+	echo "=== Cloning Nemotron Repository ==="
+	git clone https://huggingface.co/nvidia/nemotron-ocr-v1
+fi
+
+pushd nemotron-ocr-v1 >/dev/null
+git lfs pull || true
+popd >/dev/null
+
+pushd nemotron-ocr-v1/nemotron-ocr >/dev/null
+pip install --no-cache-dir -v .
+popd >/dev/null
+
+echo "=== Updating libstdcxx-ng version ==="
+conda remove libstdcxx-ng --force -y
+conda install -y -c conda-forge "libstdcxx-ng>=13"
+export LD_LIBRARY_PATH=$CONDA_PREFIX/lib:$LD_LIBRARY_PATH
 
 echo "=== Installing dependencies ==="
 pip install -r install_requirements.txt
-
-echo "=== Configuring offline settings ==="
-conda env config vars set TRANSFORMERS_OFFLINE=1 HF_HUB_OFFLINE=1 HF_DATASETS_OFFLINE=1 HF_HOME="$PWD/.hf_cache"
 
 echo "=== Setup complete ==="
