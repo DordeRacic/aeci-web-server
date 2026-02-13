80d3e19ebcbdb35975042257aeddb92d00d982f9
Dylan Bock <bockdaa@uni.edu>
2026-02-13 16:03:25 -0600

    Moved bug fix line to activation script.

--- src/runOCR.sh
+++ src/runOCR.sh
@@ -5,8 +5,5 @@
 #SBATCH --error=output.err
 #SBATCH --time=0-12:00:00
 
-# Important line, do not remove. Solves a bug with C++ library used by OCR.
-export LD_LIBRARY_PATH=$CONDA_PREFIX/lib:$LD_LIBRARY_PATH
-
 module load cuda
-srun python -u ocr.py ../../redacted-samples/Abaxis-Zoetis
+srun python -u ocr.py ../../redacted-sample/Abaxis-Zoetis
